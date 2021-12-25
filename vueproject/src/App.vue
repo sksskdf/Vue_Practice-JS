29cb8f7369c4cfdeec8874699597aa68359abeeb
Harry <tlqfk__@naver.com>
2021-12-26 01:17:47 +0900

    1

--- vueproject/src/App.vue
+++ vueproject/src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div> <!-- 템플릿 바로 밑에는 하나의 태그만 존재해야 에러가 안남-->
+    <h1>{{ title }}</h1> <!-- 데이터 바인딩-->
+    <p>{{ count }}</p>
+    <button @click="count ++">추가</button>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+    data() {
+      return{
+        title:"안녕하세요.",
+        count : 1
+      }
+    }
+  }
+</script>
